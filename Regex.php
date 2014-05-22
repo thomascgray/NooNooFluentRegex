3ea79a49a1908dc995f7da47afef0c2f3d327093
Tom Gray <tom.c.gray@gmail.com>
2014-05-22 14:53:59 +0100

    Added match class, removed test data, added namespace
    Added a match class to match text against and get your capture groups, added namespacing

--- Regex.php
+++ Regex.php
@@ -1,15 +1,14 @@
 <?php
 
-/*
-TODO
+namespace NooNoo\FluentRegex;
 
-add each name to an array and check against them so we dont allow duplicate names
-*/
 class Regex
 {
-	//the actual expression we're going to return at the end
-	protected $expression;
-		
+	const ZERO_OR_MORE = "*";
+	const ONE_OR_MORE = "+";
+	const ZERO_OR_ONE = "?";
+	
+	protected $expression;	
 	protected $multiple_value = null;
 	protected $multiple_value_limit = null;
 	
@@ -29,39 +28,55 @@ class Regex
 	 */
 	public function add($string, $name = null)
 	{
+		
 		$this->expression .= "(";
 		
-		if ($name != null)
+		if (isset($name))
 		{
-			$this->expression .= "?P<".$name.">";
+			$this->expression .= "?P<";
+			$this->expression .= $name;
+			$this->expression .= ">";			
 		}
 		
-		if (isset($multiple_string))
-		{
-			$this->expression .= $this->multiple_string;
-			$this->multiple_string = null;
-		}
-		else
-		{
-			//handle all the multiples stuff		
-			if (isset($multiple_value))
-			{
-				$this->expression .= "{" + $this->multiple_value;
-			}
+		$this->expression .= $string;
+				
+		//handle all the multiples stuff		
+		if (isset($this->multiple_value))
+		{				
+			$this->expression .= "{";
+			$this->expression .= $this->multiple_value;
 			
 			if (isset($multiple_value_limit))
 			{
-				$this->expression .= "," + $this->multiple_value . "}";
+				$this->expression .= "," + $this->multiple_value_limit . "}";
 			}
 			else
 			{
 				$this->expression .= "}";
-			}		
+			}
 			$this->multiple_value = null;
 			$this->multiple_value_limit = null;
-		}
+		}			
 		
-		$this->expression .= ")";		
+		//this also needs to be based on what's sent in - raw characters want it on the outside, ranges want it on the inside
+		switch ($this->multiple_string)
+		{
+			case '':
+				$this->expression .= $this->multiple_string;
+				$this->expression .= ")";
+				break;
+			case self::ZERO_OR_ONE :
+				$this->expression .= ")";
+				$this->expression .= $this->multiple_string;
+				break;
+			case self::ONE_OR_MORE :
+			case self::ZERO_OR_MORE :
+				$this->expression .= $this->multiple_string;
+				$this->expression .= ")";
+				break;
+		}		
+		
+		$this->multiple_string = '';
 
 		return $this;
 	}
@@ -92,7 +107,7 @@ class Regex
 	 */
 	public function oneOrMore()
 	{
-		$this->multiple_string = "+";
+		$this->multiple_string = self::ONE_OR_MORE;
 		
 		return $this;
 	}
@@ -103,7 +118,7 @@ class Regex
 	 */
 	public function zeroOrMore()
 	{
-		$this->multiple_string = "*";
+		$this->multiple_string = self::ZERO_OR_MORE;
 		
 		return $this;	
 	}
@@ -111,9 +126,9 @@ class Regex
 	/**
 	 * Defines having exactly zero or one of the next item
 	 */
-	public function zeroOrOne()
+	public function optional()
 	{
-		$this->multiple_string = "?";		
+		$this->multiple_string = self::ZERO_OR_ONE;		
 		
 		return $this;
 	}
@@ -135,29 +150,25 @@ class Regex
 	 */
 	public function end()
 	{
-		$this->expression .= '$';
-		
-		return $this;
+		return $this->expression .= '$';
 	}
 	
 	/**
 	 * Any lowercase character
 	 * @return Regex
 	 */
-	public function lowercase()
+	public function lowercase($name = null)
 	{
-		$this->add("[a-z]");
-		
-		return $this;
+		return $this->add("[a-z]", $name);
 	}
 	
 	/**
 	 * Any uppercase character
 	 * @return Regex
 	 */
-	public function uppercase()
+	public function uppercase($name = null)
 	{
-		$this->add("[A-Z]");
+		$this->add("[A-Z]", $name);
 		
 		return $this;
 	}
@@ -166,31 +177,39 @@ class Regex
 	 * Any letter of any case
 	 * @return Regex
 	 */
-	public function alpha()
+	public function alpha($name = null)
 	{
-		$this->add("[a-zA-Z]");
-		
-		return $this;
+		return $this->add("[a-zA-Z]", $name);		
 	}
 	
 	/**
 	 * Any letter, number, underscore or hyphen
 	 * @return Regex
 	 */
-	public function slugchar()
+	public function slugchar($name = null)
 	{
-		$this->add("[a-zA-Z0-9-_]");
+		$this->add("[a-zA-Z0-9-_\/]", $name);
 		
 		return $this;
 	}
 	
 	/**
-	 * Any number
+	 * Any number of any length
 	 * @return Regex
 	 */
-	public function number()
+	public function number($name = null)
 	{
-		$this->add("[0-9]");
+		$this->add("[0-9]+", $name);
+		
+		return $this;
+	}
+	
+	/**
+	 * Any single digit
+	 */
+	public function numeric($name = null)
+	{
+		$this->add("[0-9]", $name);
 		
 		return $this;
 	}
@@ -199,10 +218,138 @@ class Regex
 	 * Any alpha numeric character (letter of any case or number)
 	 * @return Regex
 	 */
-	public function alphanumeric()
+	public function alphanumeric($name = null)
 	{
-		$this->add("[a-zA-Z0-9]");
+		$this->add("[a-zA-Z0-9]", $name);
 		
 		return $this;
+	}
+	
+	/**
+	 * Basically an alias for "add"
+	 */
+	public function then($string, $name = null)
+	{
+		$this->add(preg_quote($string, "/"), $name);
+		
+		return $this;
+	}
+	
+	/**
+	 * A wrapper making something optional then doing an add
+	 */
+	public function maybe($string, $name = null)
+	{
+		$this->optional();
+		$this->add(preg_quote($string, "/"), $name);
+		
+		return $this;
+	}
+	
+	/**
+	 * Wrapper to create an optional chunk from 2 different strings
+	 */
+	public function either($string1, $string2, $name = null)
+	{
+		$this->add(preg_quote($string1, "/")."|".preg_quote($string2, "/"), $name);
+		
+		return $this;
+	}
+	
+	/**
+	 * Just get the raw regex expression
+	 */
+	public function __toString()
+	{
+		return (string)$this->expression;
+	}
+	
+	public function get()
+	{
+		return (string)$this->expression;
+	}
+	
+	/**
+	 * 
+	 * Yorkshire From Here
+	 * 
+	 * */
+	public function eyUp()
+	{
+		$this->start();
+		
+		return $this;
+	}
+	
+	public function thatllDo()
+	{
+		$this->end();
+		
+		return $this;
+	}
+	
+	public function couldAppen($string, $name = null)
+	{
+		$this->maybe($string, $name);
+		
+		return $this;
+	}
+	
+	public function oneOrTother($string1, $string2, $name = null)
+	{
+		$this->either($string1, $string2, $name);
+		
+		return $this;
+	}
+	
+	public function goOnThen($string, $name = null)
+	{
+		$this->then($string, $name);
+		
+		return $this;
+	}
+}
+
+class Match
+{
+	protected $input = '';
+	protected $pattern = '';
+	protected $groups = array();
+	
+	public function __construct($input = '', $pattern = '')
+	{
+		$this->input = $input;
+		$this->pattern = $pattern;
+		
+		preg_match("/".$pattern."/", $input, $output_array);
+		
+		$this->groups = $output_array;
+	}
+	
+	public function setInput($input)
+	{
+		$this->input = $input;
+	}
+	
+	public function getInput($input)
+	{
+		return $this->input;
+	}
+	
+	public function getGroups()
+	{
+		return $this->groups;
+	}
+	
+	public function getGroup($index_or_name)
+	{
+		if (isset($this->groups[$index_or_name]))
+		{
+			return $this->groups[$index_or_name];	
+		}
+		else
+		{			
+			throw new OutOfRangeException('Attempting to find group by index or name - Group does not exist');
+		}		
 	}
 }
